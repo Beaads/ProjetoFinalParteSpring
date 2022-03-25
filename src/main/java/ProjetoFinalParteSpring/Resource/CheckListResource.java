8e75d9fac1b706ef4a4b54d33a4d495ebc9b2673
Beaads <biacriciuma@hotmail.com>
2022-03-25 08:25:49 -0300

    Delete modificado.

--- src/main/java/ProjetoFinalParteSpring/Resource/CheckListResource.java
+++ src/main/java/ProjetoFinalParteSpring/Resource/CheckListResource.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -31,18 +32,20 @@ public class CheckListResource {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Optional> findById(@PathVariable Integer id){
+    public ResponseEntity<Optional> findById(@PathVariable Integer id) {
         return new ResponseEntity<>(checkListService.findByIdChecklist(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<CheckListRespostaDTOPost> save (@RequestBody CheckListEntity checkDto) {
+    public ResponseEntity<CheckListRespostaDTOPost> save(@RequestBody CheckListEntity checkDto) {
         return new ResponseEntity<>(checkListService.save(checkDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<?> delete(@PathVariable Integer id){
-        checkListService.delete(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    public ResponseEntity<?> remove(@PathVariable Integer id) {
+        return checkListService.findByIdChecklist(id).map(record -> {
+            checkListService.delete(id);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body("entidade nao encontrada!"));
     }
 }
